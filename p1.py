b111ca355db6491638e6368907a39decfd8739ff
MatthewCupach <mcupach6528@floridapoly.edu>
2025-11-16 03:45:30 +0000

    Part1 Finished

--- p1.py
+++ p1.py
@@ -1,5 +1,7 @@
 from nicegui import ui
 
+#the hashing code
+
 def hashCode(hashString):
     h = 0x9E3779B1
     length_of_input = len(hashString)
@@ -10,13 +12,20 @@ def hashCode(hashString):
         h = h ^ length_of_input
     return int(h)
 
-def greet(mes):
-    msg = f"Hello, stranger! " + mes
-    ui.notify(msg) # Create a popup
+#the method I used spawned the hashes in a list on the webside
+#so this is to improve said list.
+
+currentHash = 0
+def hashRun(enterStringHere):
+    global currentHash
+    currentHash = currentHash + 1
+    return f"Hash {currentHash}: {hashCode(enterStringHere)}"
+
+# the ui setup
 
 ui.label("Welcome to Hash Help!").style("color: #00FFFF; font-size: 45px")
 with ui.card():
-    input_string
-    ui.button('Find Hash', on_click=lambda: greet(input_string))
+    input_string = ui.input(label="Hash", placeholder="Enter string to hash here").style("min-width: 200px")
+    ui.button('Find Hash', on_click=lambda: ui.label(hashRun(input_string.value.strip())))
 
 ui.run(title="Hash Help")
